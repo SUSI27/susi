e2c7aab24c90ef0902b6379936581112006552ae
SUSI27 <susiladevi.cse@gmail.com>
2016-08-17 22:57:16 +0530

    Create reverse_digit.java

---
+++ reverse_digit.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner d=new Scanner(System.in);
+		int a=d.nextInt();
+		int r=0;
+		if(a>0)
+		{
+		while(a!=0)
+		{
+			r=r*10;
+			r=r+a%10;
+			a=a/10;
+		}
+		System.out.println("The reversed digit is " +r);
+		}
+		else
+		{
+			System.out.println("cannot reverse the digit");
+		}
+	}
+}
