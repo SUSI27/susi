515b5901b349589432b373107ff8712fd167c22c
SUSI27 <susiladevi.cse@gmail.com>
2016-08-18 14:38:30 +0530

    Update vowcon.java

--- vowcon.java
+++ vowcon.java
@@ -10,9 +10,16 @@ class Ideone
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		char a=sc.next().charAt(0);
+		if(Character.isLetter(a))
+		{
 		if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
 		System.out.println("The given character " +a+ " is vowel");
 		else
 		System.out.println("the given character " +a+ " is consonant");
+		}
+		else
+		{
+			System.out.println("the given value " +a+ " is not a character");
+		}
 	}
 }
