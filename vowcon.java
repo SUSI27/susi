2ced6a92433e9c4fa0465eccf4de4109146d36ac
SUSI27 <susiladevi.cse@gmail.com>
2016-08-18 14:33:15 +0530

    Create vowcon.java

---
+++ vowcon.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		char a=sc.next().charAt(0);
+		if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+		System.out.println("The given character " +a+ " is vowel");
+		else
+		System.out.println("the given character " +a+ " is consonant");
+	}
+}
