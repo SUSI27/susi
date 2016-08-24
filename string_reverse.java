8cfbd44f2f19f77590b5886041c54092201afc33
SUSI27 <susiladevi.cse@gmail.com>
2016-08-24 22:32:01 +0530

    Create string_reverse.java

---
+++ string_reverse.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		char []ch=new char[s.length()];
+		for(int i=0;i<s.length();i++)
+		ch[i]=s.charAt(i);
+		for(int i=s.length()-1;i>=0;i--)
+		System.out.print(ch[i]);
+	}
+}
