9b60246b3efade7f4faa53847aae7fe0f04091ed
SUSI27 <susiladevi.cse@gmail.com>
2016-09-01 16:09:31 +0530

    Create target.java

---
+++ target.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int target=sc.nextInt();
+		int []a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int j=0;j<n;j++)
+		{
+			for(int k=j+1;k<n-1;k++)
+			{
+				if(a[j]+a[k]==target)
+				{
+					System.out.println(a[j]);
+					System.out.println(a[k]);
+					
+				}
+				
+			}
+		}
+	}
+	
+}
