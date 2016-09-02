fcb9bd8d43426b50b9f8a1e24287347ae35f2108
SUSI27 <susiladevi.cse@gmail.com>
2016-09-02 11:01:27 +0530

    Create keypad.java

---
+++ keypad.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		if(a==2)
+		System.out.println("ABC");
+		else if(a==3)
+		System.out.println("DEF");
+		else if(a==4)
+		System.out.println("GHI");
+		else if(a==5)
+		System.out.println("JKL");
+		else if(a==6)
+		System.out.println("MNO");
+		else if(a==7)
+		System.out.println("PQRS");
+		else if(a==8)
+		System.out.println("TUV");
+		else if(a==9)
+		System.out.println("WXYZ");
+		else
+		System.out.println("invalid input");
+	
+	}
+}
